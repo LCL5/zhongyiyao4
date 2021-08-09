7c9b4027e138a2185e4f485fe71eee7deb365026
LCL5 <122877453@qq.com>
2021-08-09 22:56:48 +0800

    Initial commit

---
+++ src/main/java/com/example/zhongyiyao4/Zhongyiyao4Application.java
@@ -0,0 +1,13 @@
+package com.example.zhongyiyao4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zhongyiyao4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zhongyiyao4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zhongyiyao4/Zhongyiyao4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhongyiyao4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zhongyiyao4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
